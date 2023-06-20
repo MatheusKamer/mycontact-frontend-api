da5855cbe4a7bc2f04b7a4d9692b4b9cd22f0185
Matheus Kamer <matheus_kamer@hotmail.com>
2023-06-19 20:23:09 -0400

    Implementando status de error no e-mail (já com css)

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -13,8 +13,10 @@ export default function ContactForm({ buttonLabel }) {
       <FormGroup>
         <Input placeholder='Nome' />
       </FormGroup>
-      <FormGroup>
-        <Input placeholder='E-mail' />
+      <FormGroup
+        error={'O formato do e-mail é invalido'}
+      >
+        <Input placeholder='E-mail' error />
       </FormGroup>
       <FormGroup>
         <Input placeholder='Telefone' />
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,16 +5,11 @@ import { Link } from 'react-router-dom'
 import arrow from '../../assets/images/icons/arrow.svg'
 import edit from '../../assets/images/icons/edit.svg'
 import trash from '../../assets/images/icons/trash.svg'
-import Modal from '../../components/Modal/index.js'
-import Loader from '../../components/Loader/index.js'
 
 export default function Home() {
   return (
 
     <Container>
-      <Loader />
-      {/* <Modal danger /> */}
-
       <InputSearchContainer>
         <input type='text' placeholder='Pesquisar contato...' />
       </InputSearchContainer>
