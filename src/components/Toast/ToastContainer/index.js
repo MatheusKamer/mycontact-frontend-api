c0ea8137e2b04540a11cbfb31358da79600e3b24
Matheus Kamer <matheus_kamer@hotmail.com>
2024-08-24 11:50:01 -0400

    feat: create toast component

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,13 +1,24 @@
 import { Container } from "./styles";
 
 import ToastMessage from "../ToastMassage";
+import { useState } from "react";
 
 export default function ToastContainer() {
+  const [message, setMessage] = useState([
+    { id: Math.random(), type: "default", text: "Default toast!" },
+    { id: Math.random(), type: "danger", text: "Error toast" },
+    { id: Math.random(), type: "success", text: "Success toast" }
+  ]);
+
   return (
     <Container>
-      <ToastMessage message="Default toast!"/>
-      <ToastMessage message="Error toast" type="danger" />
-      <ToastMessage message="Success toast" type="success"/>
+      {message.map((item) => (
+        <ToastMessage
+          key={item.id}
+          type={item.type}
+          message={item.text}
+        />
+      ))}
     </Container>
   )
 }
